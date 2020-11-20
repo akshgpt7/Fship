06d4993b88db081850fa7286ee6ef1ca974a8877
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-11-20 15:32:38 +0100

    frontend

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title><h1 class="font-italic">Fship </h1></v-toolbar-title>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Fship logo"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="./assets/FSHIPLOGO.png"
+          width="60"
+          height="60"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+       @click="$router.push('/')"
+       text
+      >
+        <span class="mr-2">Home</span>
+       
+        
+      </v-btn>
+      <v-btn
+       @click="$router.push('/login')"
+       text
+      >
+        <span class="mr-2">Signup</span>
+        
+        
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+     
+        <router-view></router-view>
+     
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default {
+  name: "App",
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+
